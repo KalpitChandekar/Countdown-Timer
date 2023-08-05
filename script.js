50dfae7a73b24fde3c7cddb98dbb4686e62a3868
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-08-05 19:48:07 +0530

    add date by script

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-const endDate = '30 March 2022 10:00 PM';
+const endDate = "24 Sep 2023 10:00 PM";
 
-document.getElementById('end-date') .innerText = endDate;
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input");
+
+function clock() {
+  const end = new Date(endDate);
+  const now = new Date();
+
+  const diff = (end - now) / 1000;
+  if (diff < 0) return;
+
+  inputs[0].value = Math.floor(diff / 3600 / 24);
+  inputs[1].value = Math.floor(diff / 3600) % 24;
+  inputs[2].value = Math.floor(diff / 60) % 60;
+  inputs[3].value = Math.floor(diff) % 60;
+}
+
+clock();
+
+setInterval(() => {
+  clock();
+}, 1000);
